f7518e4dc87fe2a6cfe41ae269d1afb9622d4e3b
andi911 <2434564078@qq.com>
2015-03-25 13:40:36 +0800

    init project

---
+++ wanhui/wanhui/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wanhui
+//
+//  Created by liangyu on 15-3-25.
+//  Copyright (c) 2015年 cloudn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ wanhui/wanhuiTests/wanhuiTests.m
@@ -0,0 +1,40 @@
+//
+//  wanhuiTests.m
+//  wanhuiTests
+//
+//  Created by liangyu on 15-3-25.
+//  Copyright (c) 2015年 cloudn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface wanhuiTests : XCTestCase
+
+@end
+
+@implementation wanhuiTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
